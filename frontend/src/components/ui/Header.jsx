386134719dae205b5e518e9e909b7612090bb43e
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-03-31 19:38:21 -0300

    mobile button animation

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -1,10 +1,12 @@
-import logo from "../../assets/white_king.svg"
+import { useState } from "react"
 
+import logo from "../../assets/white_king.svg"
 const Header = () => {
   const menuItem = "relative flex h-full items-center p-4 cursor-pointer font-bold text-white hover:bg-white/10 transition-colors ease-in-out"
   const goGradient = "bg-gradient-to-r from-[#00ADD8] to-[#00A29C]"
   const logoText = "font-bold text-2xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
-
+  const line = `h-1 w-8 my-1 rounded-full bg-white transition ease transform duration-300`
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <nav className={`flex items-center top-0 ${goGradient}`}>
       <div className="flex items-center p-2 gap-2">
@@ -15,13 +17,23 @@ const Header = () => {
       </div>
 
       {/* mobile nav */}
-      <div className="block md:hidden ml-auto pr-4 my-auto cursor-pointer">
-        <div id="">
-          <div className="bg-white rounded-full w-8 h-1"></div>
-          <div className="bg-white rounded-full w-8 mt-1 h-1"></div>
-          <div className="bg-white rounded-full w-8 mt-1 h-1"></div>
-        </div>
-      </div>
+      <button className="md:hidden flex flex-col ml-auto pr-4 my-auto cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}>
+        <div
+          className={`${line} ${
+                isOpen
+                  ? "rotate-45 translate-y-3 opacity-100"
+                  : "opacity-100"
+            }`}
+        />
+        <div className={`${line} ${isOpen ? "opacity-0" : "opacity-100"}`} />
+        <div
+          className={`${line} ${
+                isOpen
+                  ? "-rotate-45 -translate-y-3 opacity-100"
+                  : "opacity-100"
+            }`}
+        />      </button>
 
       {/* desktop nav */}
       <div className="hidden md:flex flex-1 items-center justify-end">
