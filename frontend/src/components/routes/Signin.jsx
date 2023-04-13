5f35f4db8365cb07518ba89e4943682f9f46a308
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-04-12 22:03:35 -0300

    layout css

--- frontend/src/components/routes/Signin.jsx
+++ frontend/src/components/routes/Signin.jsx
@@ -2,7 +2,7 @@ import { Layout } from "../ui/Layout"
 
 const SignIn = () => ( 
   <Layout>    
-    <h1 className="text-3xl font-bold underline dark:text-white">Start a new chess game</h1>
+    <h1 className="font-bold underline dark:text-white">Start a new chess game</h1>
   </Layout>
 )
 
