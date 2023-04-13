1198e1bce8f47dc247dc4909570703ecc8c2b8c3
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-04-13 14:11:00 -0300

    fetching and rendering chess board

--- frontend/src/components/routes/Root.jsx
+++ frontend/src/components/routes/Root.jsx
@@ -1,10 +1,14 @@
-import ChessBoard from "../game/Chess"
+import ChessBoard from "../game/ChessBoard"
 import { Layout } from "../ui/Layout"
 
 const Root = () => ( 
   <Layout>    
     <h1 className="text-3xl font-bold underline">Tailwind</h1>
-    <ChessBoard/>
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="mx-auto">
+        <ChessBoard/>
+      </div>
+    </div>
   </Layout>
 )
 
