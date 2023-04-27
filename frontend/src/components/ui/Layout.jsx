ec1c8a2e03e9c5e32da0f0c3b1092cc6be4b8bb5
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-04-27 17:54:25 -0300

    adding basic footer

--- frontend/src/components/ui/Layout.jsx
+++ frontend/src/components/ui/Layout.jsx
@@ -2,7 +2,7 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export const Layout = ({children}) => (
-<div className="min-h-screen bg-color overflow-hidden">
+    <div className="min-h-screen bg-color overflow-hidden">
         <Header/>
         <main>{children}</main>
         <Footer/>
