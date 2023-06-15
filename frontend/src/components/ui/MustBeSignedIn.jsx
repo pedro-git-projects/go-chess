4d54ed3917803d129ede46e4fa5a6305b5ac65ef
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-06-15 19:40:34 -0300

    changing a to useNavigete #68

--- frontend/src/components/ui/MustBeSignedIn.jsx
+++ frontend/src/components/ui/MustBeSignedIn.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const MustBeSignedIn = () => {
+	const navigate = useNavigate()
+	const handleClick = () => navigate("/signin")
 return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-4">You must be signed in to play</h1>
-      <a href="/signin" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div onClick={handleClick} href="/signin" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Sign In
-      </a>
+      </div>
     </div>
   )
 }
